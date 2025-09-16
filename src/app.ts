6ab39841ea071a4263fd2604d656db9fbfcd6d37
shivamjjha <44032634+shivamjjha@users.noreply.github.com>
2025-09-16 20:44:30 +0530

    feat: loading env, creating jwt token on /sign endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import dotenv from "dotenv"
+dotenv.config()
 import express from "express"
 import { authRouter } from "./routes/auth.ts"
 const app = express()
@@ -5,6 +7,13 @@ const port = 5000
 
 app.get("/", (req, res) => {
   res.send("Hello World!")
+})
+
+app.use(express.json())
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).json({ stack: err.stack, message: err.message })
 })
 
 app.use("/auth", authRouter)
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,24 @@
 import express from "express"
+import jwt from "jsonwebtoken"
+
 const router = express.Router()
 
-router.use("/", (_, res) => {
-  res.status(200).json({ hello: "world" })
+router.post("/sign", (req, res) => {
+  console.log("params", req.body, req.params)
+  jwt.sign(
+    req.body.data,
+    process.env.JWT_SECRET!,
+    {
+      expiresIn: "1h",
+      algorithm: "HS512",
+    },
+    function (err, token) {
+      if (err) {
+        throw err
+      }
+      res.status(200).send(token)
+    },
+  )
 })
 
 export { router as authRouter }
