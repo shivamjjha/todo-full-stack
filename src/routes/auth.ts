80809c39c00bd62f35b96cb0adf20f54c5892160
shivamjjha <44032634+shivamjjha@users.noreply.github.com>
2025-09-16 20:53:01 +0530

    feat: sending token as json obj

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,22 +3,29 @@ import jwt from "jsonwebtoken"
 
 const router = express.Router()
 
+const EXPIRES_HOUR = 1
 router.post("/sign", (req, res) => {
   console.log("params", req.body, req.params)
   jwt.sign(
     req.body.data,
     process.env.JWT_SECRET!,
     {
-      expiresIn: "1h",
+      expiresIn: `${EXPIRES_HOUR}h`,
       algorithm: "HS512",
     },
     function (err, token) {
-      if (err) {
+      if (err || !token) {
         throw err
       }
-      res.status(200).send(token)
+      res.status(200).send({
+        token,
+        // expires:
+        //   Date.now() + EXPIRES_HOUR * 60 * 60 * 1000 /* - 0.5 * 60 * 1000 */,
+      }) // expires in: (reduce 30s for safety)
     },
   )
 })
+
+// router.post("/verify")
 
 export { router as authRouter }
