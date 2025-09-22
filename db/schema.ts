ab8d0e987131689a84f7b2f6a84b43e588b1fa5d
shivamjjha <44032634+shivamjjha@users.noreply.github.com>
2025-09-22 15:53:05 +0530

    feat: users table

--- db/schema.ts
+++ db/schema.ts
@@ -2,7 +2,9 @@ import { int, sqliteTable, text } from "drizzle-orm/sqlite-core"
 
 export const usersTable = sqliteTable("users_table", {
   id: int().primaryKey({ autoIncrement: true }),
+  slug: text().notNull().unique(), // public facing primary key (kinda)
   name: text().notNull(),
-  age: int().notNull(),
-  email: text().notNull().unique(),
+  username: text().notNull().unique(),
+  email: text().unique(),
+  password: text().notNull(),
 })
