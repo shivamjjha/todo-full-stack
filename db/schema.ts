0c7fd6d20ab3e5a6cdce26b5051aee465174af87
shivamjjha <44032634+shivamjjha@users.noreply.github.com>
2025-09-22 18:48:37 +0530

    feat: schema for todos table with relation to users table

--- db/schema.ts
+++ db/schema.ts
@@ -1,3 +1,4 @@
+import { relations } from "drizzle-orm"
 import { int, sqliteTable, text } from "drizzle-orm/sqlite-core"
 
 export const usersTable = sqliteTable("users_table", {
@@ -7,4 +8,30 @@ export const usersTable = sqliteTable("users_table", {
   username: text().notNull().unique(),
   email: text().unique(),
   password: text().notNull(),
+})
+
+export const todosTable = sqliteTable("todos_table", {
+  id: int().primaryKey({ autoIncrement: true }),
+  userId: int()
+    .notNull()
+    .references(() => usersTable.id),
+  title: text().notNull(),
+  description: text(),
+  status: text().default("incomplete"), // incomplete, in-progress, complete
+})
+
+// user -> todos (1 to many)
+export const usersRelation = relations(usersTable, ({ many }) => {
+  return {
+    todos: many(todosTable),
+  }
+})
+
+export const todosRelation = relations(todosTable, ({ one }) => {
+  return {
+    owner: one(usersTable, {
+      fields: [todosTable.userId],
+      references: [usersTable.id],
+    }),
+  }
 })
