8fba9b7bbe06eb775eaf594ea4596506a211052c
shivamjjha <44032634+shivamjjha@users.noreply.github.com>
2025-09-25 12:17:10 +0530

    feat: serving static files, index.html init for UI, added local db to gitignore

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,22 @@
 import dotenv from "dotenv"
 dotenv.config()
 import express from "express"
+import { fileURLToPath } from "url"
 import { authRouter } from "./routes/auth.ts"
 import { todosRouter } from "./routes/todos.ts"
 import { authMiddleware } from "./middlewares/auth.ts"
+import path from "path"
 const app = express()
 const port = 5000
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 app.get("/", (req, res) => {
   res.send("Hello World!")
 })
 
 app.use(express.json())
+console.log(path.join(__dirname, "ui"))
+app.use(express.static(path.join(__dirname, "ui")))
 
 app.use("/auth", authRouter)
 app.use("/todos", authMiddleware, todosRouter)
